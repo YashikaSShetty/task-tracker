257612d82534a49f853c294ab84340aeaf6717e0
YashikaSShetty <yashikashetty15@gmail.com>
2025-04-12 22:17:23 +0530

    feat: added frontend with React + Axios

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [tasks,setTasks]=useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/tasks")
+    .then((res)=>{setTasks(res.data)})
+    .catch((err)=>{console.log(err)});
+  },[]);
+  return (
+    <div style={{padding:"20px"}}>
+      <h2>Task Tracker</h2>
+      <ul>
+        {tasks.map((task)=>
+        <li key={task._id}> 
+          {task.title} - {task.completed ? "✅" : "❌"} 
+        </li>)}
+      </ul>
+    </div>
+/*     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div> */
+  );
+}
+
+export default App;
