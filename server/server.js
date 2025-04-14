df4dfbc1235fbb776f395098220df84ace8242b4
YashikaSShetty <yashikashetty15@gmail.com>
2025-04-14 22:36:30 +0530

    Added POST and DELETE task routes

--- server/server.js
+++ server/server.js
@@ -11,9 +11,36 @@ const tasks = [
   { id: 2, title: "Build Task Tracker", completed: false }
 ];
 
+//get
 app.get('/api/tasks', (req, res) => {
   res.json(tasks);
 });
+
+//post added
+app.post  ('/api/tasks', (req, res) => {
+  const {title} = req.body;
+  if (!title) {
+    return res.status(400).json({ message: 'Title is required' });
+  }
+  const newTask = {
+    id: tasks.length + 1,
+    title,
+    isCompleted: false
+  };
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+})
+
+//delete added
+app.delete('/api/tasks/:id', (req, res) => {
+  const id = req.params.id;
+  const taskIndex = tasks.findIndex(task => task.id === parseInt(id)); 
+  if (taskIndex === -1) {
+    return res.status(404).json({ message: 'Task not found' });
+  }
+  tasks.splice(taskIndex, 1);
+  return res.status(204).send();  // Respond with 204 No Content to indicate successful deletion
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
